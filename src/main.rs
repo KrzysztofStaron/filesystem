8b6378ad12fd6c9075bc4b51b853a60e300462f0
krzysztofstaron <kisiel3141@gmail.com>
2026-03-08 21:07:54 +0100

    retard

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-fn main() {
-    println!("{}", min_operations("0100".to_string()));
-    println!("{}", min_operations("10".to_string()));
-    println!("{}", min_operations("1111".to_string()));
-}
+use std::collections::HashSet;
 
-// s1: 0101
-// s2: 1010
-
-fn min_operations(s: String) -> i32 {
-    let mut changes_s1 = 0;
-    let mut changes_s2 = 0;
-
-    for (index, c) in s.chars().enumerate() {
-        let expected_s1 = if index % 2 == 0 { '0' } else { '1' };
-        let expected_s2 = if index % 2 == 0 { '1' } else { '0' };
-
-        if c != expected_s1 {
-            changes_s1 += 1;
+pub fn find_different_binary_string(nums: Vec<String>) -> String {
+    let present : HashSet<u16> = nums.iter().map(|n| u16::from_str_radix(n, 2).unwrap()).collect();
+    let max: usize = 2_usize.pow(nums.len() as u32);
+    
+    for n in (0..max).rev() {
+        if !present.contains(&(n as u16)) {
+            return format!("{:0width$b}", n, width = nums.len());
         }
-
-        if c != expected_s2 {
-            changes_s2 += 1;
-        }
-
     }
 
-    return changes_s1.min(changes_s2);
+    "".to_string()
+}
+
+fn main() {
+    let input = vec!["01".to_string(), "10".to_string()];
+    let result = find_different_binary_string(input);
+    println!("{}", result);
 }
