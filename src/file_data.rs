f4e2b96a3ed130f0e448fa7ba7981b00713397de
krzysztofstaron <kisiel3141@gmail.com>
2026-03-09 22:04:09 +0100

    standarized disc

--- src/file_data.rs
+++ src/file_data.rs
@@ -1,7 +1,8 @@
 use crate::main::BLOCK_SIZE;
+use crate::main::DISC_NAME;
 
 fn readFileData(start: u32, length: u32) -> Vec<u8> {
-    let mut f = std::fs::File::open("test.img").unwrap();
+    let mut f = std::fs::File::open(DISC_NAME.unwrap());
     let mut bytes = Vec::new();
     f.read_to_end(&mut bytes).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod utils;
 pub const BLOCK_SIZE: usize = 512; // in bytes
 pub const DISC_SIZE_BLOCKS: usize = 2048; // blocks
 pub const DISC_SIZE_BYTES: usize = BLOCK_SIZE*DISC_SIZE_BLOCKS;
+pub const DISC_NAME: &str = "mydisk.img";
 
 fn main() {
     let test_file = FileHeader {
@@ -33,14 +34,18 @@ fn main() {
         content: vec![test_file, test_file_2]
     };
 
+    
+    let mut bytes = file_system_header.serialize();
 
+    while bytes.len() < DISC_SIZE_BYTES {
+        bytes.push(0);
+    }
 
-    let bytes = file_system_header.serialize();
-    let mut f = std::fs::File::create("test.img").unwrap();
+    let mut f = std::fs::File::create(DISC_NAME).unwrap();
     f.write_all(&bytes).unwrap();
     drop(f);
 
-    let mut f = std::fs::File::open("test.img").unwrap();
+    let mut f = std::fs::File::open(DISC_NAME).unwrap();
     let mut bytes = Vec::new();
     f.read_to_end(&mut bytes).unwrap();
     drop(f);
